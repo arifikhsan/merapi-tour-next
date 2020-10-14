d6b26bcbf23a8deb3295bd3ad8c6cc7c716bbc29
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-10-14 20:37:34 +0700

    placeholder

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import Nav from '../components/nav'
-
 export default function IndexPage() {
   return (
     <div>
-      <Nav />
       <div className="py-20">
-        <h1 className="text-5xl text-center text-accent-1">
-          Next.js + Tailwind CSS Hello
+        <h1 className="text-5xl text-center text-green-500">
+          Awesome Merapi
         </h1>
       </div>
     </div>
