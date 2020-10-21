0e3417131957153a0c56af5901d141be85dc8258
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-10-21 08:05:27 +0700

    hero

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
 export default function IndexPage() {
   return (
     <div>
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-green-500">
-          Awesome Merapi  aaa
-        </h1>
+      {/* hero */}
+      <div className="">
+        <div className="relative h-screen">
+          <div className="absolute inset-0 z-10">
+            <div className="flex items-center justify-center w-full h-full p-4 text-center">
+              <h1 className="text-5xl font-bold text-white">Awesome Merapi</h1>
+            </div>
+          </div>
+          <div className="absolute z-0">
+            <img
+              className="object-cover w-full h-screen"
+              src="/image/1.jpg"
+              alt="bg"
+            />
+          </div>
+        </div>
       </div>
+
+      {/* what to do */}
+      <div>whattodo</div>
+      {/* gallery */}
+      {/* visit & accomodation */}
+      {/* footer */}
     </div>
-  )
+  );
 }
