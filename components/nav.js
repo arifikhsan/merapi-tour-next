da8affa55d09736da42d5126cfad47d5a2485d08
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-10-21 14:45:39 +0700

    separate nav

--- components/nav.js
+++ components/nav.js
@@ -1,29 +1,114 @@
-import Link from 'next/link'
+import Link from "next/link";
+import React from "react";
+import { Component } from "react";
 
-const links = [
-  { href: 'https://github.com/vercel/next.js', label: 'GitHub' },
-  { href: 'https://nextjs.org/docs', label: 'Docs' },
-]
+export default class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: true,
+    };
+  }
 
-export default function Nav() {
-  return (
-    <nav>
-      <ul className="flex justify-between items-center p-8">
-        <li>
-          <Link href="/">
-            <a className="text-blue-500 no-underline">Home</a>
-          </Link>
-        </li>
-        <ul className="flex justify-between items-center space-x-4">
-          {links.map(({ href, label }) => (
-            <li key={`${href}${label}`}>
-              <a href={href} className="btn-blue no-underline">
-                {label}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </ul>
-    </nav>
-  )
+  toggleMenu = () => {
+    this.setState({
+      menu: !this.state.menu,
+    });
+  };
+
+  render() {
+    const { menu } = this.state;
+
+    return (
+      <div className="w-full font-display">
+        {/* header */}
+        <div className="z-20 w-full">
+          <div className="flex justify-between font-display">
+            <div className="flex items-center justify-center p-4">
+              <Link href="/">
+                <p className="font-bold text-white">Merapi Tour</p>
+              </Link>
+            </div>
+            <div
+              onClick={this.toggleMenu}
+              className="p-4 text-white transition duration-500 cursor-pointer hover:bg-gray-600"
+            >
+              <svg
+                className="w-8 h-8"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+
+        {/* overlay menu */}
+        {menu && (
+          <div className="absolute inset-0 z-30 bg-white">
+            <div className="flex flex-col h-full">
+              <div className="relative flex items-center justify-between">
+                <div></div>
+                <div
+                  onClick={this.toggleMenu}
+                  className="absolute top-0 right-0 p-4 text-gray-800 transition duration-500 cursor-pointer hover:bg-gray-600 hover:text-white"
+                >
+                  <svg
+                    className="w-8 h-8"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div className="text-xl font-bold text-gray-600 font-display">
+                <div className="grid grid-cols-2">
+                  <div
+                    style={{ height: `50vh` }}
+                    className="flex items-center justify-center transition duration-500 cursor-pointer hover:text-white hover:bg-gray-500"
+                  >
+                    What To Do
+                  </div>
+                  <div
+                    style={{ height: `50vh` }}
+                    className="flex items-center justify-center transition duration-500 cursor-pointer hover:text-white hover:bg-gray-500"
+                  >
+                    Accomodation
+                  </div>
+                  <div
+                    style={{ height: `50vh` }}
+                    className="flex items-center justify-center transition duration-500 cursor-pointer hover:text-white hover:bg-gray-500"
+                  >
+                    Gallery
+                  </div>
+                  <div
+                    style={{ height: `50vh` }}
+                    className="flex items-center justify-center transition duration-500 cursor-pointer hover:text-white hover:bg-gray-500"
+                  >
+                    Contact Us
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,116 +1,16 @@
 import React from "react";
 import { Component } from "react";
+import Nav from "../components/nav";
 
 export default class IndexPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menu: false,
-    };
-  }
-
-  toggleMenu = () => {
-    this.setState({
-      menu: !this.state.menu,
-    });
-  };
-
   render() {
-    const { menu } = this.state;
-
     return (
       <div className="font-body">
-        {/* header */}
-        <div className="fixed z-20 w-full">
-          <div className="flex justify-between font-display">
-            <div className="flex items-center justify-center p-4">
-              <p className="font-bold text-white">Merapi Tour</p>
-            </div>
-            <div
-              onClick={this.toggleMenu}
-              className="p-4 text-white transition duration-500 cursor-pointer hover:bg-gray-600"
-            >
-              <svg
-                className="w-8 h-8"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h7"
-                />
-              </svg>
-            </div>
-          </div>
-        </div>
-
-        {/* overlay menu */}
-        {menu && (
-          <div className="fixed inset-0 z-30 bg-white">
-            <div className="flex flex-col h-full">
-              <div className="relative flex items-center justify-between">
-                <div></div>
-                <div
-                  onClick={this.toggleMenu}
-                  className="absolute top-0 right-0 p-4 text-gray-800 transition duration-500 cursor-pointer hover:bg-gray-600 hover:text-white"
-                >
-                  <svg
-                    className="w-8 h-8"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </div>
-              </div>
-              <div className="text-xl font-bold text-gray-600 font-display">
-                <div className="grid grid-cols-2">
-                  <div
-                    style={{ height: `50vh` }}
-                    className="flex items-center justify-center transition duration-500 cursor-pointer hover:text-white hover:bg-gray-500"
-                  >
-                    What To Do
-                  </div>
-                  <div
-                    style={{ height: `50vh` }}
-                    className="flex items-center justify-center transition duration-500 cursor-pointer hover:text-white hover:bg-gray-500"
-                  >
-                    Accomodation
-                  </div>
-                  <div
-                    style={{ height: `50vh` }}
-                    className="flex items-center justify-center transition duration-500 cursor-pointer hover:text-white hover:bg-gray-500"
-                  >
-                    Gallery
-                  </div>
-                  <div
-                    style={{ height: `50vh` }}
-                    className="flex items-center justify-center transition duration-500 cursor-pointer hover:text-white hover:bg-gray-500"
-                  >
-                    Contact Us
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
-
         {/* hero */}
         <div className="relative min-h-screen">
           <div className="absolute inset-0 z-10">
-            <div className="flex flex-col items-center justify-between w-full h-full p-4 text-center">
-              <div></div>
+            <div className="flex flex-col items-center justify-between w-full h-full pb-4 text-center">
+              <Nav />
               <h1 className="text-5xl font-extrabold text-white md:text-6xl font-display">
                 Awesome Merapi
               </h1>
