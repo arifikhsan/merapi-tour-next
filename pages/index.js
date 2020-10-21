d32b39400668ee24e2b3b8ab3da46dbc581f3b24
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-10-21 08:30:24 +0700

    wtd

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,12 @@ export default function IndexPage() {
     <div className="font-body">
       {/* hero */}
       <div className="">
-        <div className="relative h-screen">
+        <div className="relative min-h-screen">
           <div className="absolute inset-0 z-10">
             <div className="flex items-center justify-center w-full h-full p-4 text-center">
-              <h1 className="text-5xl font-black text-white font-display">Awesome Merapi</h1>
+              <h1 className="text-5xl font-extrabold text-white font-display">
+                Awesome Merapi
+              </h1>
             </div>
           </div>
           <div className="absolute z-0">
@@ -20,7 +22,12 @@ export default function IndexPage() {
       </div>
 
       {/* what to do */}
-      <div>whattodo</div>
+      <div className="min-h-screen">
+        <div className="flex items-center justify-center text-center">
+          <h2>What To Do in Merapi</h2>
+        </div>
+        <div>Hiking</div>
+      </div>
       {/* gallery */}
       {/* visit & accomodation */}
       {/* footer */}
