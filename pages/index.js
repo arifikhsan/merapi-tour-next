b808fa433cdbd02bec2534d25343782706d1e516
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-10-21 08:26:00 +0700

    fonts

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 export default function IndexPage() {
   return (
-    <div>
+    <div className="font-body">
       {/* hero */}
       <div className="">
         <div className="relative h-screen">
           <div className="absolute inset-0 z-10">
             <div className="flex items-center justify-center w-full h-full p-4 text-center">
-              <h1 className="text-5xl font-bold text-white">Awesome Merapi</h1>
+              <h1 className="text-5xl font-black text-white font-display">Awesome Merapi</h1>
             </div>
           </div>
           <div className="absolute z-0">
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,18 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      display: "Montserrat",
+      body: "Inter",
+    },
     extend: {
       colors: {
-        'accent-1': '#333',
+        "accent-1": "#333",
       },
     },
   },
   variants: {},
   plugins: [],
-}
+};
